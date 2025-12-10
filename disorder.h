0e32616fa9116bfc581d0151fb97af2496a2d6ee
Boyoung Kim <bokim@z2r10p8.42lyon.fr>
2025-12-10 16:53:56 +0100

    feat: simple algo implemented

--- disorder.h
+++ disorder.h
@@ -8,6 +8,25 @@ typedef struct stack
 	struct stack *next;
 }	stack;
 
+//bench struct
+typedef struct bench
+{
+	double	*disorder;
+	char	*strategy;
+	int		*total_op;
+	int		*sa_count;
+	int		*sb_count;
+	int		*ss_count;
+	int		*pa_count;
+	int		*pb_count;
+	int		*ra_count;
+	int		*rb_count;
+	int		*rr_count;
+	int		*rra_count;
+	int		*rrb_count;
+	int		*rrr_count;
+}	bench;
+
 //libft
 #include "libft/libft.h"
 
